7a14cdc44794da741c762d3201b1d6bada28f220
Nahuel Federico <alegrenahuel@gmail.com>
2019-12-22 09:34:21 -0300

    se termino formulario, conectando con API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import WeatherInfo from './components/WeatherInfo';
+import WeatherForm from './components/WeatherForm';
 
-function App() {
+class App extends Component {
+
+  getWeather = e => {
+    const {city, country } = e.target.elements;
+    const cityValue = city.value;
+    const countryValue = country.value;
+    console.log(cityValue, countryValue)
+    e.preventDefault();
+    
+  }
+  render(){
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-4 mx-auto">
+          <WeatherForm getWeather={this.getWeather}/>
+          <WeatherInfo />
+        </div>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
