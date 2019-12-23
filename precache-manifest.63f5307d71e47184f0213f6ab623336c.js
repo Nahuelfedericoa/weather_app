d7529b2609e0a84751f894ce5b18eaaf5cba3dad
Nahuel Federico <alegrenahuel@gmail.com>
2019-12-23 14:38:37 -0300

    Updates

---
+++ precache-manifest.63f5307d71e47184f0213f6ab623336c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa148325fdd36662f0e0e401af132bcf",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "28703dc5a0d87ab5ab72",
+    "url": "/weather_app/static/css/main.97b9d715.chunk.css"
+  },
+  {
+    "revision": "a3d91142019994479178",
+    "url": "/weather_app/static/js/2.dec2e9f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather_app/static/js/2.dec2e9f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28703dc5a0d87ab5ab72",
+    "url": "/weather_app/static/js/main.8593a03d.chunk.js"
+  },
+  {
+    "revision": "5db99314f8fe7bf85fe1",
+    "url": "/weather_app/static/js/runtime-main.5c949abe.js"
+  }
+]);
